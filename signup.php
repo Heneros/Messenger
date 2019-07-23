7afe11355174b7405e2e4f69ee02b046b29a2673
Rustam <punk60826@gmail.com>
2019-07-23 18:24:13 +0300

    first commit

---
+++ signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Messenger</title>
+</head>
+<body>
+    <div class="signup-container">
+        <div class="account-left">
+         <h1>Account Left</h1>
+        </div>
+        <div class="account-right">
+            <div class="form-area">
+                <form action="" method="POST">
+                    <div class="group">
+                    <input type="text" name="full_name" class="control" placeholder="Enter your name"></div>
+                    <div class="group">
+                    <input type="email" name="email" class="control" placeholder="Enter your email"></div>
+                    <div class="group">
+                    <input type="password" name="password" class="control" placeholder="Enter your password"></div>
+
+                    <div class="group">
+                    <label for="file" id="file-label"> Choose image</label>
+                    <input type="file" name="img" class="file" id="file"></div>
+                    <div class="group">
+ 
+                    <input type="submit" name="signup" class="btn signup-btn" value="Submit"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
