e91c9ef6319cea92ff19caaf844f850611642f4f
Rustam <punk60826@gmail.com>
2019-07-28 18:55:22 +0300

    added .htaccess starting work on index page styling

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Messenger</title>
+</head>
+<body>
+<nav id="nav">
+</nav>
+<div class="chat-container">
+    <section id="sidebar">
+    </section>
+    <section id="right-area">
+    <div class="messages">
+    messages area
+    </div>
+    <div class="chat-form">
+    chat form
+    </div>
+    </section>
+</div>
+
+
+</body>
+</html>
