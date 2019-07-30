97d81c42e61f1c0b06403575a6ae0b7d22d15922
Rustam <punk60826@gmail.com>
2019-07-30 18:20:00 +0300

    message area design

--- index.php
+++ index.php
@@ -12,11 +12,131 @@
 </nav>
 <div class="chat-container">
     <section id="sidebar">
+    <ul class="left-ul">
+        <li><a href="#!"> <span class="profile-img-span"><img src="img/homer.png" class="profile-img" alt="profile-img"></span></a></li>
+        <li><a href="#!">Homer Simpson <span class="cool-hover"></span></a></li>
+        <li><a href="#!">Change Name <span class="cool-hover"></span></a></li>
+        <li><a href="#!">Change Password <span class="cool-hover"></span></a></li>
+        <li><a href="#!">110 User Online <span class="cool-hover"></span></a></li>
+    </ul>
     </section>
     <section id="right-area">
-    <div class="messages">
-    messages area
+     <div class="messages">
+    <div class="left-message common-margin">
+    <div class="sender-img-block">
+    <img src="img/bender.jpg" class="sender-img" alt="sender image">
     </div>
+    <div class="left-msg-area">
+        <div class="user-name-date">
+            <span class="sender-name">
+            Bender Rodriguez 
+            </span>
+            <span class="date-time">
+                1 day ago
+            </span>
+         </div>
+         <div class="left-msg">
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+         </div>
+       </div>
+    </div>
+    <div class="left-message common-margin">
+    <div class="sender-img-block">
+    <img src="img/bender.jpg" class="sender-img" alt="sender image">
+    </div>
+    <div class="left-msg-area">
+        <div class="user-name-date">
+            <span class="sender-name">
+            Bender Rodriguez 
+            </span>
+            <span class="date-time">
+                1 day ago
+            </span>
+         </div>
+         <div class="left-msg">
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+         </div>
+       </div>
+    </div>
+    <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
+        <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
+        <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
+        <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
+        <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
+        <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
+        <div class="left-message common-margin">
+    <div class="sender-img-block">
+    <img src="img/bender.jpg" class="sender-img" alt="sender image">
+    </div>
+    <div class="left-msg-area">
+        <div class="user-name-date">
+            <span class="sender-name">
+            Bender Rodriguez 
+            </span>
+            <span class="date-time">
+                1 day ago
+            </span>
+         </div>
+         <div class="left-msg">
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+         </div>
+       </div>
+    </div>
+ </div>
     <div class="chat-form">
     chat form
     </div>
