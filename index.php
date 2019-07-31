77b1c69caf57861593a88885e348faaf1e3ad18d
Rustam <punk60826@gmail.com>
2019-07-31 23:57:18 +0300

    scrollbar added  and icons offline and online

--- index.php
+++ index.php
@@ -35,14 +35,19 @@
                 1 day ago
             </span>
          </div>
+         
          <div class="left-msg">
              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
          </div>
        </div>
+       
     </div>
     <div class="left-message common-margin">
     <div class="sender-img-block">
     <img src="img/bender.jpg" class="sender-img" alt="sender image">
+    <span class="online-icon">
+    
+    </span>
     </div>
     <div class="left-msg-area">
         <div class="user-name-date">
@@ -118,10 +123,23 @@
             </div>
              </div>
         </div>
+        <div class="right-messages common-margin">
+             <div class="right-msg-area">
+             <span class="date-time right-time">
+                1 day ago
+            </span>
+            <div class="right-msg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
+            </div>
+             </div>
+        </div>
         <div class="left-message common-margin">
     <div class="sender-img-block">
     <img src="img/bender.jpg" class="sender-img" alt="sender image">
+    <span class="offline-icon">
+    </span>   
     </div>
+    
     <div class="left-msg-area">
         <div class="user-name-date">
             <span class="sender-name">
@@ -130,13 +148,16 @@
             <span class="date-time">
                 1 day ago
             </span>
-         </div>
+         </div><!-- user-name-date-end -->
          <div class="left-msg">
              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestiae expedita sed!</p>
-         </div>
-       </div>
+             </div> <!-- left-msg-end -->
+       </div><!-- left-msg-area -->
+
     </div>
+    
  </div>
+ 
     <div class="chat-form">
     chat form
     </div>
