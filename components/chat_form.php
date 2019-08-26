616560ec760dec9b01a6f7ba6659965d275a5c14
Rustam <punk60826@gmail.com>
2019-08-26 15:24:37 +0300

    send emoji and files in db

--- components/chat_form.php
+++ components/chat_form.php
@@ -8,7 +8,9 @@
               <label for="upload-files" id="upload-label"><i class="fas fa-paperclip fa-uploads"></i><i class="fas fa-file-image fa-uploads"></i></label>
              <input type="file" 
              id="upload-files"
-             class="files-upload" >
+             class="files-upload"
+             name="send_file">
           </div><!-- form-input close -->
       </div> <!-- chat-container close --> 
+      <div class="files-error"></div>
     </form><!-- chat-form close -->
--- js/chat.js
+++ js/chat.js
@@ -1,24 +1,5 @@
-// $(document).on("click", "#save", function () {
-//     var message = $("#send_message").val();
-//     if (message == "") {
-//         $("#error_message").html("Please enter message");
-//         return false;
-//     } else {
-//         $("#error_message").html("");
-//     }
-//     $.ajax({
-//         type: "POST",
-//         url: 'ajax/send_message.php',
-//         data: {message: message},
-//         cache: false,
-//         success: function (data) {
-//             alert(data);
-//             $("#message").val("");
-//         }
-//     });
-// });
-
 $(document).ready(function(){
+    ///send messages
     $(".chat-form").keypress(function(e){
         if(e.keyCode == 13) {
             var message = $("#send_message").val();
@@ -46,31 +27,44 @@ $(document).ready(function(){
                 })
         }
     })
+///send files
+    $("#upload-files").change(function(){
+        var file_name = $("#upload-files").val();
+        if(file_name.length != ""){
+            $.ajax({
+                type: 'POST',
+                url: 'ajax/send_files.php',
+                data: new FormData($(".chat-form")[0]),
+                contentType: false,
+                processData: false,
+                success: function(feedback){
+                    if(feedback == "error") {
+                         $(".files-error").addClass("show-file-error");
+                         $(".files-error").html("<span class='files-cross-icon'>&#x2715;</span>"+ "Please choose valid image/file");
+                         setTimeout(function(){
+                            $(".files-error").removeClass("show-file-error");
+                         }, 4000)
+                    }else if(feedback == "success"){
+                        alert('file/image send');
+                    }
+                }
+            })
+        }
+    })
+    ///send emoji
+    $(".emoji-same").click(function(){
+        var emoji = $(this).attr("src");
+        $.ajax({
+            type: 'POST',
+            url: 'ajax/send_emoji.php',
+            data: {'send_emoji': emoji},
+            dataType: 'JSON',
+            success: function(feedback){
+                if(feedback.status == "success"){
+                    alert("emoji send");
+                }
+            }
+        })
+    })
 })
-
-// $(document).ready(function(){
-//     $(".chat-form").keypress(function(e){
-//         if(e.keyCode == 13) {
-//             var send_message = $("#send_message").val();
-//             if(send_message.length != "" ){
-//                                  $.ajax({
-//                     type: 'POST',
-//                     url: 'ajax/send_message.php',
-//                     data: {send_message:send_message},
-//                     dataType: 'JSON',
-//                     success: function(feedback){
-//                         if(feedback.status == "success"){
-//                             alert(feedback);
-//                         }
-//                     }
-//                     // error: function(error){
-//                     //     console.log(error);
-//                     // }
-//                 })
-            
-//             }
-
-//         }
-//     })
-// })
 
